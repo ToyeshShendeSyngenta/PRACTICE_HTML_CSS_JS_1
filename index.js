e518890400f48c3e1a209474aecc78893b38d1ec
toyesh <toyeshshende02@gmail.com>
2024-01-22 11:39:09 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,61 +1,49 @@
-
-
-
-const cardContainer = document.getElementById('cardContainer');
+const cardContainer = document.getElementById("cardContainer");
 for (let i = 1; i <= 9; i++) {
-  const listItem = document.createElement('li');
-  listItem.className = 'card';
+  const listItem = document.createElement("li");
+  listItem.className = "card";
   listItem.innerHTML = `<span>${i}</span>`;
   cardContainer.appendChild(listItem);
 }
 
-const cards = Array.from(cardContainer.querySelectorAll('.card'));
+const cards = Array.from(cardContainer.querySelectorAll(".card"));
 
+const shuffleButton = document.getElementById("shuffle");
 
-const shuffleButton = document.getElementById('shuffle');
-
-shuffleButton.addEventListener('click', () => {
+shuffleButton.addEventListener("click", () => {
   cards.forEach((card) => {
     const randomIndex = Math.floor(Math.random() * cards.length);
     cardContainer.insertBefore(card, cardContainer.children[randomIndex]);
   });
 });
 
-
-const sortButton = document.getElementById('sort');
-
+const sortButton = document.getElementById("sort");
 
 const sortCards = () => {
   cards.sort((a, b) => a.textContent - b.textContent);
-  cardContainer.append(...cards); 
+  cardContainer.append(...cards);
 };
 
-const c = document.querySelectorAll('.card');
-
+const c = document.querySelectorAll(".card");
 
 function applyStyles() {
-    const screenWidth = window.innerWidth;
+  const screenWidth = window.innerWidth;
 
-    c.forEach((c, index) => {
-        if (screenWidth <= 375) {
-            
-            c.style.background = `linear-gradient(to right, var(--color-${index + 1}) 10%, white 0%, #efefef 100%)`;
-        } else {
-            
-            c.style.background = `var(--color-${index + 1})`;
-        }
-    });
+  c.forEach((c, index) => {
+    if (screenWidth <= 375) {
+      c.style.background = `linear-gradient(to right, var(--color-${
+        index + 1
+      }) 10%, white 0%, #efefef 100%)`;
+    } else {
+      c.style.background = `var(--color-${index + 1})`;
+    }
+  });
 }
-
-
 
 applyStyles();
 
-
-
-
-sortButton.addEventListener('click', sortCards);
-window.addEventListener('resize', applyStyles);
+sortButton.addEventListener("click", sortCards);
+window.addEventListener("resize", applyStyles);
 
 /* git init
 git add .
@@ -65,6 +53,6 @@ git remote add origin https://github.com/ToyeshShendeSyngenta/PRACTICE_HTML_CSS.
 git push origin main */
 
 // git push --set-upstream origin master
-// git add . 
+// git add .
 // git commit -m "second commit"
 // git push
